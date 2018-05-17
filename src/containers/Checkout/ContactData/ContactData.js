93b7461e9455df233e05992783d4b8c72520f0b2
ESlB <eslevano@hotmail.com>
2018-05-17 00:05:32 -0500

    Initial files

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
+
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        console.log(this.props.ingredients);
+        //axios.post('orders.json', order).then(response=>{this.setState(); this.props.history.push('/')}).catch(error=>{this.setState({loading: false })});
+        this.props.history.push('/');
+    }
+
+    render() {
+        return(
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                <form>
+                    <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
+                    <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/> 
+                    <input className={classes.Input} type="text" name="street" placeholder="Street"/>
+                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ContactData;
