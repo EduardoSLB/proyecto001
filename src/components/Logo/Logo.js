e4cddc879992c607c629675d8d07f489a98be4e9
EduardoSLB <travstqdsm@gmail.com>
2018-08-07 21:53:18 -0500

    Antes de redireccionar BD

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip'
+import icono from './imagen2.jpe'
 //import tarmaLogo from '../../assets/images/catedral.jpg';
 //import classes from './Logo.css';
 
 const logo = (props) =>(
     <div >
         
-        <img style={{height: "45px" }} data-tip="© 2018 Eduardo Lévano"   src="http://www.catedralastorga.com/wp-content/uploads/DSC0814-1-630x420.jpg" alt="Comunidad de Tarma"/>
+        <img style={{height: "45px" }} data-tip="© 2018 Eduardo Lévano"   src={icono} alt="Comunidad de Tarma"/>
         <ReactTooltip/>
     </div>
 );
